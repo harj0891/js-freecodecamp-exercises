0550b615896c69986f9bd740f8bbcacd0a6e09cf
Harj <harjinder.chahal@spika.com>
2021-03-23 18:23:36 +0000

    palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function() {
+const palindromes = function(sentence) {
+
+    let origSentenceArr = sentence.toLowerCase().match(/\w/ig);
+    let revSentenceArr = [...origSentenceArr].reverse();
+    
+    let originalSentence = origSentenceArr.join("");
+    let revSentence = revSentenceArr.join("");
+
+    
+    return (originalSentence === revSentence) ? true : false;
 
 }
 
