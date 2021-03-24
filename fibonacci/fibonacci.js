396e2ff28b8211d70b978828e7708c82f124a0e0
Harj <harjinder.chahal@spika.com>
2021-03-24 12:55:12 +0000

    fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciIndex) {
+
+    let fibonacciArray = [];
+
+    if(fibonacciIndex < 0) {
+        return "OOPS";
+    }
+
+    for (let i=0; i < fibonacciIndex; i++) {     
+        let currfibonacci;
+        if (i==0 || i==1) {
+            fibonacciArray.push(1);
+        } else {
+            currfibonacci = fibonacciArray[i-1] + fibonacciArray[i-2];
+            fibonacciArray.push(currfibonacci);
+        }
+    }
+
+
+
+    return fibonacciArray[fibonacciIndex-1];
 
 }
+
+console.log("test");
+console.log(fibonacci(6));
 
 module.exports = fibonacci
